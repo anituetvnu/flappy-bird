35305d72570207023c16b4de84138f4266c29d42
anituetvnu <ngovanan182000@gmail.com>
2020-04-27 23:28:49 +0700

    ferpect move of bird

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import pygame
+import math
+import numpy
 
 pygame.font.init()
 
@@ -9,10 +11,11 @@ pygame.display.set_caption("Flappy bird")
 
 clock = pygame.time.Clock()
 tick = 1
-x = 10
+x = 130
 y = 300
-g = 17  # acceleration
-vel = 10
+g = 30  # acceleration
+basevel = 20
+vel = basevel
 last_click = 0
 
 # load images
@@ -31,24 +34,37 @@ BIRD_IMAGE = [
 
 
 class Bird():
-    pass
+    basevely = 10
+
+    def __init__(self):
+        velx = basevely
+        vely = basevely
+        g = 10
+        angel = 0
+        x = 130
+        y = 300
+        time = 0
+
+    def update(self, time):
+        y += - basevely * time + 1/2 * g**2
+        angel = -numpy.arctan((-vel + g * time)/vel) /math.pi * 180
 
 
-print(BIRD_IMAGE[0])
 running = True
 while running:
     clock.tick(24)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        else:
-            print(pygame.time.get_ticks())
+        if event.type == pygame.KEYDOWN:
+            print("A keystoke is pressed")
+            print('last_click:{} this_click:{}'.format(last_click, pygame.time.get_ticks()))
             last_click = pygame.time.get_ticks()
-            vel = 10
-    SCREEN.fill((255, 255, 255))
+            vel = basevel
+    SCREEN.blit(BACKGROUND_IMAGE[0], (0, 0))
     tick += 1
     time = (pygame.time.get_ticks() - last_click)/1000
     y += -vel * time + 1/2 * g * time**2
-
-    SCREEN.blit(BIRD_IMAGE[tick % 3], (x, y))
+    angel = -numpy.arctan((-vel + g * time)/20) / math.pi * 180
+    SCREEN.blit(pygame.transform.rotate(BIRD_IMAGE[tick % 3], angel), (x, y))
     pygame.display.update()
